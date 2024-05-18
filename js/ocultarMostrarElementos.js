39c94f9152d28b377094cb78f6af23e361a4a86e
jesusksa <ksajesusedu@gmail.com>
2024-05-18 20:50:29 -0300

    Index: mejorado ultimo carrusel para que se oculte y muestre otro cuando se achique la pantalla

---
+++ js/ocultarMostrarElementos.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function toggleCarouselVisibility() {
+        var contenedor2 = document.getElementById("contenedor2");
+        if (window.innerWidth < 800) {
+            contenedor2.style.display = "none";
+        } else {
+            contenedor2.style.display = "block";
+        }
+    }
+
+    // Run the function on initial load
+    toggleCarouselVisibility();
+
+    // Run the function whenever the window is resized
+    window.addEventListener("resize", toggleCarouselVisibility);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    function toggleCarouselVisibility() {
+        var contenedor3 = document.getElementById("contenedor3");
+        if (window.innerWidth < 800) {
+            contenedor3.style.display = "block";
+        } else {
+            contenedor3.style.display = "none";
+        }
+    }
+
+    // Run the function on initial load
+    toggleCarouselVisibility();
+
+    // Run the function whenever the window is resized
+    window.addEventListener("resize", toggleCarouselVisibility);
+});
